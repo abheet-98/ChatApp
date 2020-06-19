41cce79762cbfe708c1fdc75bc2bfd4f502d2529
DESKTOP-4PHBDCU\91822 <abheet.gera01@gmail.com>
2020-06-19 16:14:12 +0530

    Added display picture

--- flutter_chat_ui_starter-master/lib/widgets/request_tile.dart
+++ flutter_chat_ui_starter-master/lib/widgets/request_tile.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:bethere_app/models/request.dart';
@@ -26,7 +27,8 @@ class RequestTile extends StatelessWidget {
       child: GestureDetector(
         onTap: onTap,
         child: Container(
-          margin:EdgeInsets.only(top: 5.0, bottom: 5.0, right: 10.0, left: 5.0),
+          margin:
+              EdgeInsets.only(top: 5.0, bottom: 5.0, right: 10.0, left: 5.0),
           padding: EdgeInsets.symmetric(horizontal: 1.0, vertical: 5.0),
           decoration: BoxDecoration(color: Colors.blueGrey[50]),
           child: Row(
@@ -34,10 +36,26 @@ class RequestTile extends StatelessWidget {
             children: <Widget>[
               Row(
                 children: <Widget>[
-//                  CircleAvatar(
-//                    radius: 35.0,
-//                    backgroundImage: AssetImage(request.url),
-//                  ),
+                  CachedNetworkImage(
+                    //https://ik.imagekit.io/bethere/tr:w-500,c-fill,q-auto/public/556b7519-4e7.jpeg?ik-sdk-version=python-2.2.4
+                    imageUrl:
+                        "https://ik.imagekit.io/bethere/tr:w-500,c-fill,q-auto/public/556b7519-4e7.jpeg?ik-sdk-version=python-2.2.4",
+                    placeholder: (context, url) =>
+                        Center(child: CircularProgressIndicator()),
+                    errorWidget: (context, url, error) => Image.asset(
+                      "assets/images_main/blank-profile-picture.png",
+                      fit: BoxFit.cover,
+                      width: MediaQuery.of(context).size.width / 5.2,
+                      height: MediaQuery.of(context).size.height / 7,
+                    ),
+                    width: MediaQuery.of(context).size.width / 5.2,
+                    height: MediaQuery.of(context).size.height / 7,
+                    fit: BoxFit.cover,
+                  ),
+                  /* CircleAvatar(
+                    radius: 35.0,
+                    backgroundImage: AssetImage(request.url),
+                  ), */
                   SizedBox(
                     width: 10.0,
                   ),
